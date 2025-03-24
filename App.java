ec5ff9a222c740f05ea7cc898bacf7fa2eaee04d
cesar <cesarks81@gmail.com>
2025-03-24 09:42:24 +0100

    Se realizaron cambios

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
 public class App {
   
   public static void main(String[] args) {
-    System.out.println("Contenido 2a");
+    System.out.println("César Ramos Morón");
+    System.out.println("Contenido 3b");
+    System.out.println("Contenido 2c");
     System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
+    System.out.println("Ies San Andrés");
   }
   
 }
